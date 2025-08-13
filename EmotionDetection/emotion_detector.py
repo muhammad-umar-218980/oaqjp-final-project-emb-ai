3e85abf4e7f8f1edbac9a56afe5bb55db6986f0e
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-13 14:56:51 -0400

    Packaged application into EmotionDetection

---
+++ EmotionDetection/emotion_detector.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    data = response.json()  
+
+    emotions = data['emotionPredictions'][0]['emotion']
+    anger_score = emotions.get('anger', 0)
+    disgust_score = emotions.get('disgust', 0)
+    fear_score = emotions.get('fear', 0)
+    joy_score = emotions.get('joy', 0)
+    sadness_score = emotions.get('sadness', 0)
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
